ddc0f68fd137996b7c5e51269447183f4ce1708c
DZM77 <32109758+DZM77@users.noreply.github.com>
2025-05-21 14:45:58 +0200

    Add another project and some more debugg changes

--- GIT/Program.cs
+++ GIT/Program.cs
@@ -6,10 +6,27 @@ namespace GIT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
 
+            int a = 1;
+            int b = 2;
+
+            int res = Sum(a, b);
+
+            Console.WriteLine(res);
+
+            int[] numbers = Enumerable.Range(1, 100).ToArray();
+
+            foreach (var nr in numbers)
+            {
+                int x = nr;
+            }
+
+        }
+
+        private static int Sum(int a, int b)
+        {
+            return a + b;
         }
     }
 }
